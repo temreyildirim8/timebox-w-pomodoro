d73f12d33c698e8c828995c3309ac5cff8672ff2
Emre Yildirim <temreyildirim8@gmail.com>
2026-04-27 16:17:49 +0200

    fix: vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? "/Timebox/" : "/",
+  base: process.env.NODE_ENV === 'production' ? "/timebox-w-pomodoro/" : "/",
   plugins: [
     react(),
     VitePWA({
